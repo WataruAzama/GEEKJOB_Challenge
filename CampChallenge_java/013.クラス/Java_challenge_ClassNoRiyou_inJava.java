caa44f298d2721418ab63a4e7ffb635c4f7562ba
WataruAzama <azmwtr67@gmail.com>
2018-04-08 09:02:34 +0900

    Java・クラスの利用inJavaの課題を提出します。

---
+++ CampChallenge_java/013.クラス/Java_challenge_ClassNoRiyou_inJava.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Sanosuke
+ */
+    
+//クラス作成
+class Tate{
+    public String name ="";
+    public int age = 0;
+    
+    //メソッド1
+    void hoge(String n,int a){
+        
+        this.name = n;
+        this.age = a;
+        
+        
+    }
+    
+    //メソッド2
+    void fuga(PrintWriter pw){
+        //インスタンス生成
+        
+        
+        pw.print(this.name);
+        pw.print(this.age);
+        
+    }
+}
+
+
+public class Java_challenge_ClassNoRiyou_inJava extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            Tate ito = new Tate();
+            ito.hoge("miyuki",45);
+            ito.fuga(out);
+            
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Java_challenge_ClassNoRiyou_inJava</title>");            
+            out.println("</head>");
+            out.println("<body>");
+
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
