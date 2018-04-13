582991d74745b14c0080fcf86f5073385087908b
WataruAzama <azmwtr67@gmail.com>
2018-04-13 16:00:34 +0900

    Java・標準クラス・文字数の取得を提出します。

---
+++ CampChallenge_java/015.標準クラス/Mojisuunoshutoku.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sanosuke
+ */
+public class Mojisuunoshutoku {
+    public static void main(String[] args){        
+    
+    String mail = "watapachi67@yahoo.co.jp";
+    
+        int i = (mail.indexOf("@"));
+        System.out.print(mail.substring(i+1));
+    
+    }
+}
